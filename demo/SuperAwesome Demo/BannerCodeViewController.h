d46b3c56cbac12bd9629a2458ef57ec18a85e987
Balázs Kiss <dev@balazskiss.name>
2015-04-21 15:17:55 +0100

    Separate sections

---
+++ demo/SuperAwesome Demo/BannerCodeViewController.h
@@ -0,0 +1,13 @@
+//
+//  BannerCodeViewController.h
+//  SuperAwesome Demo
+//
+//  Created by Balázs Kiss on 21/04/15.
+//  Copyright (c) 2015 SuperAwesome Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BannerCodeViewController : UIViewController
+
+@end

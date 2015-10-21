0068f6044e870d7c2e3b8d95fba7165af49bae5f
Gabriel Coman <dev.gabriel.coman@gmail.com>
2015-10-21 13:17:53 +0100

    Tested delegates as well

--- demo/SuperAwesome Demo/TestVCViewController.m
+++ demo/SuperAwesome Demo/TestVCViewController.m
@@ -9,7 +9,7 @@
 #import "TestVCViewController.h"
 #import "SuperAwesome.h"
 
-@interface TestVCViewController () <SALoaderProtocol>
+@interface TestVCViewController () <SALoaderProtocol, SAViewProtocol, SAVideoAdProtocol>
 
 @property (nonatomic, strong) SAInterstitialAd *inter;
 @property (nonatomic, strong) SAFullscreenVideoAd *fvid;
@@ -40,10 +40,11 @@
 
 - (IBAction)button2Action:(id)sender {
     _fvid.isParentalGateEnabled = YES;
+    _fvid.delegate = self;
+    _fvid.videoDelegate = self;
     [self presentViewController:_fvid animated:YES completion:^{
          [_fvid playPreloaded];
     }];
-   
 }
 
 - (void) didPreloadAd:(SAAd *)ad forPlacementId:(NSInteger)placementId {
@@ -61,14 +62,60 @@
     NSLog(@"Failed");
 }
 
-/*
-#pragma mark - Navigation
+#pragma mark View Protocol
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void) adWasShown:(NSInteger)placementId {
+    NSLog(@"Ad %ld was shown", placementId);
 }
-*/
+
+- (void) adFailedToShow:(NSInteger)placementId {
+    NSLog(@"Ad %ld failed to show", placementId);
+}
+
+- (void) adWasClosed:(NSInteger)placementId {
+    NSLog(@"Ad %ld was closed", placementId);
+}
+
+- (void) adFollowedURL:(NSInteger)placementId {
+    NSLog(@"Ad %ld went to URL", placementId);
+}
+
+- (void)parentalGateWasCanceled:(NSInteger)placementId {
+    NSLog(@"Ad %ld canceled parental gate", placementId);
+}
+
+- (void)parentalGateWasFailed:(NSInteger)placementId {
+    NSLog(@"Ad %ld failed parental gate", placementId);
+}
+
+- (void)parentalGateWasSucceded:(NSInteger)placementId {
+    NSLog(@"Ad %ld OK parental gate", placementId);
+}
+
+#pragma mark Video Protocol
+
+- (void) videoStarted:(NSInteger)placementId {
+    NSLog(@"DELEGATE VIDEO STARTED %ld", placementId);
+}
+
+- (void) videoEnded:(NSInteger)placementId {
+    NSLog(@"DELEGATE VIDEO ENDED %ld", placementId);
+}
+
+- (void) videoReachedFirstQuartile:(NSInteger)placementId {
+    NSLog(@"DELEGATE VIDEO 1/4 %ld", placementId);
+}
+
+- (void) videoReachedMidpoint:(NSInteger)placementId {
+    NSLog(@"DELEGATE VIDEO 1/2 %ld", placementId);
+}
+
+- (void) videoReachedThirdQuartile:(NSInteger)placementId {
+   NSLog(@"DELEGATE VIDEO 3/4 %ld", placementId);
+}
+
+- (void) videoSkipped:(NSInteger)placementId {
+   NSLog(@"DELEGATE VIDEO SKIPPED %ld", placementId);
+}
 
 @end

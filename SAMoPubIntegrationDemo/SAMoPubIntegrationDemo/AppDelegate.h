45459d07c0cebb82fff13778383f0ee22244e1fe
Gabriel Coman <dev.gabriel.coman@gmail.com>
2015-10-26 11:53:38 +0000

    Update with mopub integration demo

---
+++ SAMoPubIntegrationDemo/SAMoPubIntegrationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SAMoPubIntegrationDemo
+//
+//  Created by Gabriel Coman on 26/10/2015.
+//  Copyright © 2015 Gabriel Coman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SAMoPubIntegrationDemo/SAMoPubIntegrationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SAMoPubIntegrationDemo
+//
+//  Created by Gabriel Coman on 26/10/2015.
+//  Copyright © 2015 Gabriel Coman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

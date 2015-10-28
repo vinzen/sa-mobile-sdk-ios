560a2d3557e0dd0fc81a416f80e171953881ddab
Gabriel Coman <dev.gabriel.coman@gmail.com>
2015-10-28 13:02:32 +0000

    Added Pods files to the repo as well

--- demo/Pods/Headers/Public/SuperAwesome/SAPadlockView.h
+++ demo/Pods/Headers/Public/SuperAwesome/SAPadlockView.h
@@ -1,18 +1,1 @@
-//
-//  SAPadlockView.h
-//  Pods
-//
-//  Created by Gabriel Coman on 08/09/2015.
-//
-//
-
-#import <UIKit/UIKit.h>
-
-/**
- *
- * The Padlock View - which actually is build to cover the whole screen
- * It loads interface elements from SAPadlockView.xib
- *
- */
-@interface SAPadlockView : UIView
-@end
+../../../../../SuperAwesome/Classes/Padlock/SAPadlockView.h
--- demo/Pods/Headers/Public/SuperAwesome/UIAlertController+Window.h
+++ demo/Pods/Headers/Public/SuperAwesome/UIAlertController+Window.h
@@ -1,16 +1,1 @@
-//
-//  UIAlertController+Window.h
-//  Pods
-//
-//  Created by Gabriel Coman on 24/09/2015.
-//
-//
-
-#import <Foundation/Foundation.h>
-
-@interface UIAlertController (Window)
-
-- (void)show;
-- (void)show:(BOOL)animated;
-
-@end
+../../../../../SuperAwesome/Classes/UIAlertController+Window.h
